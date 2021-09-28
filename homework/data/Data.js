5d86a0fa1234171890c1481a0fe58c474e5cb07a
KANGMIN\ISPARK <dfg65r@naver.com>
2021-09-28 13:59:05 +0900

    2021.09.28 체크박스, 차트 데이터 물림  근데 차트 상태가 이상함.

---
+++ homework/data/Data.js
@@ -0,0 +1,346 @@
+var Data = [
+    // grid data
+    {
+        CarMaker: "현대",
+        M1: 2500,
+        M2: 3000,
+        M3: 4000,
+        M4: 3000,
+        M5: 3700,
+        M6: 2800,
+        M7: 5000,
+        M8: 7500,
+        M9: 3400,
+        M10: 2200,
+        M11: 1300,
+        M12: 1000
+    }, {
+        CarMaker: "기아",
+        M1: 2400,
+        M2: 2500,
+        M3: 3000,
+        M4: 2800,
+        M5: 4000,
+        M6: 3200,
+        M7: 4800,
+        M8: 6100,
+        M9: 2800,
+        M10: 2000,
+        M11: 1400,
+        M12: 1200
+    }, {
+        CarMaker: "테슬라",
+        M1: 2800,
+        M2: 3000,
+        M3: 2900,
+        M4: 3100,
+        M5: 2500,
+        M6: 3400,
+        M7: 5000,
+        M8: 5500,
+        M9: 6700,
+        M10: 7000,
+        M11: 7700,
+        M12: 6500
+    }, {
+        CarMaker: "현대1",
+        M1: 2500,
+        M2: 3000,
+        M3: 4000,
+        M4: 3000,
+        M5: 3700,
+        M6: 2800,
+        M7: 5000,
+        M8: 7500,
+        M9: 3400,
+        M10: 2200,
+        M11: 1300,
+        M12: 1000
+    }, {
+        CarMaker: "기아1",
+        M1: 2500,
+        M2: 3000,
+        M3: 4000,
+        M4: 3000,
+        M5: 3700,
+        M6: 2800,
+        M7: 5000,
+        M8: 7500,
+        M9: 3400,
+        M10: 2200,
+        M11: 1300,
+        M12: 1000
+    }, {
+        CarMaker: "테슬라1",
+        M1: 2500,
+        M2: 3000,
+        M3: 4000,
+        M4: 3000,
+        M5: 3700,
+        M6: 2800,
+        M7: 5000,
+        M8: 7500,
+        M9: 3400,
+        M10: 2200,
+        M11: 1300,
+        M12: 1000
+    }, {
+        CarMaker: "반도체",
+        M1: 9800,
+        M2: 9500,
+        M3: 9750,
+        M4: 7000,
+        M5: 6830,
+        M6: 6000,
+        M7: 3200,
+        M8: 3100,
+        M9: 2840,
+        M10: 2700,
+        M11: 2230,
+        M12: 1400
+    },
+
+    // tree data
+
+    {
+        id: 'Korea',
+        parent: '#',
+        text: '국산차',
+        state: {
+            selected: true
+        }
+    }, {
+        id: 'import',
+        parent: '#',
+        text: '수입차',
+        state: {
+            selected: true
+        }
+    }, {
+        id: '1Htree',
+        parent: 'Korea',
+        text: '현대'
+    }, {
+        id: '2Ktree',
+        parent: 'Korea',
+        text: '기아'
+    }, {
+        id: '3Ttree',
+        parent: 'import',
+        text: '테슬라'
+    }, {
+        id: '4Htree1',
+        parent: 'Korea',
+        text: '현대1'
+    }, {
+        id: '5Ktree1',
+        parent: 'Korea',
+        text: '기아1'
+    }, {
+        id: '6Ttree1',
+        parent: 'import',
+        text: '테슬라1'
+    }, {
+        id : '7Mtree',
+        parent: '#',
+        text: '반도체',
+        state: {
+            selected: true
+        }
+    },
+
+    // chart data
+
+    {
+        type: 'column',
+        name: '현대',
+        color: 'skyblue',
+        data: [2500, 3000, 4000, 3000, 3700, 2800, 5000, 7500, 3400, 2200, 1300, 1000],
+        stack: '0' /* stack이 데이터값을 분류해줍니다. */
+    }, {
+        type: 'column',
+        name: '기아',
+        color: 'gray',
+        data: [2400, 2500, 3000, 2800, 4000, 3200, 4800, 6100, 2800, 2000, 1400, 1200],
+        stack: '0'
+    }, {
+        type: 'column',
+        name: '테슬라',
+        color: '#E70D00',
+        data: [2800, 3000, 2900, 3100, 2500, 3400, 5000, 5500, 6700, 7000, 7700, 6500],
+        stack: '0'
+    }, {
+        type: 'column',
+        name: '현대1',
+        color: 'skyblue',
+        data: [2500, 3000, 4000, 3000, 3700, 2800, 5000, 7500, 3400, 2200, 1300, 1000],
+        stack: '1',
+        showInLegend: true /* false : 범례에 표시하지 않음. */
+    }, {
+        type: 'column',
+        name: '기아1',
+        color: 'gray',
+        data: [2500, 3000, 4000, 3000, 3700, 2800, 5000, 7500, 3400, 2200, 1300, 1000],
+        stack: '1',
+        showInLegend: true
+    }, {
+        type: 'column',
+        name: '테슬라1',
+        color: '#E70D00',
+        data: [2500, 3000, 4000, 3000, 3700, 2800, 5000, 7500, 3400, 2200, 1300, 1000],
+        stack: '1',
+        showInLegend: true
+    }, {
+        type: 'line',
+        name: '아날로그 반도체',
+        color: '#5062FF',
+        yAxis: 1,
+        /* 두번째 y축 선택 */
+        data: [9800, 9500, 9750, 7000, 6830, 6000, 3200, 3100, 2840, 2700, 2230, 1400],
+        tooltip: {
+            valueSuffix: ' EA'
+        },
+        dataLabels: {
+            enabled: true /* 데이터라벨 표시 */
+        },
+        label: {
+            enabled: false /* 라인그래프에 라벨 숨기기 */
+        }
+    }
+]
+
+// ----------------Tree Data-------------------
+
+function isTree(element) {
+    if (element.id == 'Korea' || element.id == 'import' || element.id == '1Htree' || element.id == '2Ktree' || element.id == '3Ttree' || element.id == '4Htree1' || element.id == '5Ktree1' || element.id == '6Ttree1' || element.id == '7Mtree') {
+        return true;
+    }
+}
+
+var isTree = Data.filter(isTree);
+
+console.log(isTree);
+
+// ------------- Chart Data --------------
+
+function isChart(element) {
+    if(element.name == '현대' || element.name == '기아' || element.name == '테슬라' || element.name == '현대1' || element.name == '기아1' || element.name == '테슬라1' || element.name == '아날로그 반도체') {
+        return true;
+    }
+}
+
+var isChart = Data.filter(isChart);
+
+console.log(isChart);
+
+function isChartH(element) {
+    if(element.name == '현대') {
+        return true;
+    }
+}
+
+function isChartK(element) {
+    if(element.name == '기아') {
+        return true;
+    }
+}
+
+function isChartT(element) {
+    if(element.name == '테슬라') {
+        return true;
+    }
+}
+
+function isChartH1(element) {
+    if(element.name == '현대1') {
+        return true;
+    }
+}
+
+function isChartK1(element) {
+    if(element.name == '기아1') {
+        return true;
+    }
+}
+
+function isChartT1(element) {
+    if(element.name =='테슬라1') {
+        return true;
+    }
+}
+
+function isChartM(element) {
+    if(element.name == '아날로그 반도체') {
+        return true;
+    }
+}
+
+var isChartH = Data.filter(isChartH);
+var isChartK = Data.filter(isChartK);
+var isChartT = Data.filter(isChartT);
+var isChartH1 = Data.filter(isChartH1);
+var isChartK1 = Data.filter(isChartK1);
+var isChartT1 = Data.filter(isChartT1);
+var isChartM = Data.filter(isChartM);
+
+// -------------- Grid Data --------------
+
+function isGrid (element) {
+    if(element.CarMaker == '현대' || element.CarMaker == '기아' || element.CarMaker == '테슬라' || element.CarMaker == '현대1' || element.CarMaker == '기아1' || element.CarMaker == '테슬라1' || element.CarMaker == '반도체') {
+        return true;
+    }
+}
+
+var isGrid = Data.filter(isGrid);
+
+console.log(isGrid);
+
+function isGridH(element) {
+    if(element.CarMaker == '현대') {
+        return true;
+    }
+}
+
+function isGridK(element) {
+    if(element.CarMaker == '기아') {
+        return true;
+    }
+}
+
+function isGridT(element) {
+    if(element.CarMaker == '테슬라') {
+        return true;
+    }
+}
+
+function isGridH1(element) {
+    if(element.CarMaker == '현대1') {
+        return true;
+    }
+}
+
+function isGridK1(element) {
+    if(element.CarMaker == '기아1') {
+        return true;
+    }
+}
+
+function isGridT1(element) {
+    if(element.CarMaker == '테슬라1') {
+        return true;
+    }
+}
+
+function isGridM(element) {
+    if(element.CarMaker == '반도체') {
+        return true;
+    }
+}
+
+var isGridH = Data.filter(isGridH);
+var isGridK = Data.filter(isGridK);
+var isGridT = Data.filter(isGridT);
+var isGridH1 = Data.filter(isGridH1);
+var isGridK1 = Data.filter(isGridK1);
+var isGridT1 = Data.filter(isGridT1);
+var isGridM = Data.filter(isGridM);
