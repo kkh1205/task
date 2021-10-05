f8bfcd2d9bad80fa3c0fda8eb6b7cfbe51335f7a
KANGMIN\ISPARK <dfg65r@naver.com>
2021-10-05 13:36:30 +0900

    ExcelFile Downloader 완성

--- homework/exceltest.js
+++ homework/exceltest.js
@@ -1,4 +1,3 @@
-
 const Excel = require('exceljs');
 
 // 관리 객체
@@ -6,30 +5,56 @@ const workbook = new Excel.Workbook();
 // 엑셀 시트 생성
 const sheet = workbook.addWorksheet('Test1');
 // 열 정의
-sheet.columns = [
-    {header:'maker'},
-    {header:'1월'},
-    {header:'2월'},
-    {header:'3월'},
-    {header:'4월'},
-    {header:'5월'},
-    {header:'6월'},
-    {header:'7월'},
-    {header:'8월'},
-    {header:'9월'},
-    {header:'10월'},
-    {header:'11월'},
-    {header:'12월'},
+sheet.columns = [{
+        header: 'maker'
+    },
+    {
+        header: '1월'
+    },
+    {
+        header: '2월'
+    },
+    {
+        header: '3월'
+    },
+    {
+        header: '4월'
+    },
+    {
+        header: '5월'
+    },
+    {
+        header: '6월'
+    },
+    {
+        header: '7월'
+    },
+    {
+        header: '8월'
+    },
+    {
+        header: '9월'
+    },
+    {
+        header: '10월'
+    },
+    {
+        header: '11월'
+    },
+    {
+        header: '12월'
+    },
 ];
 // 행 생성
 sheet.addRows([
-    ['현대',2500,3000,4000,3000,3700,2800,5000,7500,3400,2200,1300,1000],
-    ['기아',2400,2500,3000,2800,4000,3200,4800,6100,2800,2000,1400,1200]
+    ['현대', 2500, 3000, 4000, 3000, 3700, 2800, 5000, 7500, 3400, 2200, 1300, 1000],
+    ['기아', 2400, 2500, 3000, 2800, 4000, 3200, 4800, 6100, 2800, 2000, 1400, 1200]
 ]);
 // 파일로 출력
-workbook.xlsx.writeFile('./Test.xlsx').then(_=>{
+
+workbook.xlsx.writeFile('./Test.xlsx').then(_ => {
     console.log('완료');
-}).catch(_=>{
+}).catch(_ => {
     console.log('실패');
 });
 
